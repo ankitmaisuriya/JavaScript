458a79940dd072cefb01b76c62720e21b6b2e9b2
Ankitkumar Maisura <ankitmaisuriya.canada@gmail.com>
2025-05-02 17:49:31 -0400

    JavaScript Committs

---
+++ script.js
@@ -0,0 +1,27 @@
+//console.log("Hello, World!");
+
+
+// let score = 8;
+
+// if (score > 0 && score < 10) {
+//     console.log("Mid-level skills: " + true)
+// } else {
+//     console.log("Mid-level skills: " + false)
+// }
+//try {
+// console.log(a + b);
+// // }
+// // catch (error) {
+// //     console.log("Error: " + error.message);
+// // }
+// console.log("Hello, World!");
+// console.log('Hello');
+
+let greeting = "Hello, World!";
+let welcome = "Welcome to JavaScript!";
+console.log(greeting + " " + welcome);
+console.log(`${greeting} Thanks! ${welcome}`);
+console.log(`${5 + 1}`);
+console.log(`Hello world!`);
+
+5 + 1;
